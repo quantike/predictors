3509648dbb17f4118f14abb2852c387fd3aafcf9
Ike <isaac.chasse@valkyrie.ai>
2024-07-08 15:34:37 -0500

    small change to docs + rebase

--- data/src/model/market.rs
+++ data/src/model/market.rs
@@ -3,12 +3,12 @@ use std::fmt::Display;
 
 /// Identifies the type of market, which affects its payout structure.
 pub enum MarketType {
-    /// Every binary market has two sides, YES and NO. If the market's "payout criterion" is
+    /// Every [`MarketType::Binary`] has two sides, YES and NO. If the market's "payout criterion" is
     /// satisfied, it pays out the notional value to holders of YES. Otherwise, it pays out the
     /// notional holders of NO.
     Binary,
 
-    /// Every scalar market has two sides, LONG and SHORT (although these might be referred to as
+    /// Every [`MarketType::Scalar`] has two sides, LONG and SHORT (although these might be referred to as
     /// YES/NO in some API endpoints). At settlement, each contract's notional value is split
     /// between LONG and SHORT as described by the rules.
     Scalar,
